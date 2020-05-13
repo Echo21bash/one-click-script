13fc86d70034bb324fec935336f4bd43fee7a648
hebaodan <hebaodan21@qq.com>
2020-05-13 16:36:46 +0800

    updata

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -47,8 +47,8 @@ auto_ssh_keygen(){
 		expect <<-EOF
 		spawn ssh-copy-id -i /root/.ssh/id_rsa.pub root@${host} -p ${ssh_port[$i]}
 		expect {
-			"yes/no" {send "yes\r";exp_continue}
-			"password" {send "${passwd[$i]}\r";exp_continue}
+			"yes/no" { send "yes\n";exp_continue}
+			"password" { send "$pass_word\n"}
         }
 		EOF
 		if [[ $? = 0 ]];then
