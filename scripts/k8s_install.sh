5e70ce689251957bc76439fa20679dd3eefd4673
hebaodan <hebaodan21@qq.com>
2020-05-13 16:19:46 +0800

    updata

--- scripts/k8s_install.sh
+++ scripts/k8s_install.sh
@@ -3,6 +3,9 @@
 env_load(){
 	. ${workdir}/config/k8s/k8s.conf
 	auto_ssh_keygen
+	tmp_dir=/tmp/install_tmp
+	mkdir -p ${tmp_dir}
+	cd ${tmp_dir}
 }
 
 install_cfssl(){
@@ -17,12 +20,12 @@ create_etcd_ca(){
 	
 	cfssl gencert -initca ${workdir}/config/k8s/ca-csr.json | cfssljson -bare ca -
 	cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=${workdir}/config/k8s/ca-config.json -profile=www ${workdir}/config/k8s/etcd-csr.json | cfssljson -bare etcd
-	cp ca*pem etcd*pem ${etcd_dir}/ssl
+	
 }
 
 
 down_k8s_file(){
-	down_file https://mirrors.huaweicloud.com/etcd/v3.2.30/etcd-v3.2.30-linux-arm64.tar.gz /tmp/etcd-v3.2.30-linux-arm64.tar.gz
+	down_file https://mirrors.huaweicloud.com/etcd/v3.2.30/etcd-v3.2.30-linux-arm64.tar.gz ${tmp_dir}/etcd-v3.2.30-linux-arm64.tar.gz
 }
 
 send_file(){
@@ -31,7 +34,7 @@ send_file(){
 	for host in ${host_name[@]};
 	do
 		if [[ ${host} = '${etcd_ip[$j]}' ]];then
-			scp /tmp/etcd-v3.2.30-linux-arm64.tar.gz root@${host}:/tmp -P ${ssh_port[i]}
+			scp ${tmp_dir}/etcd-v3.2.30-linux-arm64.tar.gz root@${host}:/tmp -P ${ssh_port[i]}
 			((j++))
 		fi
 		((i++))
@@ -41,9 +44,9 @@ send_file(){
 install_etcd(){
 	
 	mkdir -p ${etcd_dir}/{bin,cfg,ssl}
-	cd ${etcd_dir}
+	cd /tmp
 	tar zxvf etcd-v3.2.12-linux-amd64.tar.gz
-	mv ${etcd_dir}/etcd-v3.2.30-linux-arm64/{etcd,etcdctl} ${etcd_dir}/bin/
+	mv etcd-v3.2.30-linux-arm64/{etcd,etcdctl} ${etcd_dir}/bin/
 	rm -rf etcd-v3.2.12-linux-amd64.tar.gz etcd-v3.2.30-linux-arm64
 	
 }
@@ -56,6 +59,9 @@ etcd_conf(){
 		for host in ${host_name[@]};
 		do
 			if [[ ${host} = '${etcd_ip}' ]];then
+				send_file
+				install_etcd
+				add_system
 				ssh ${host_name[$i]} -p ${ssh_port[$i]}
 				cat >>${etcd_dir}/cfg/etcd.conf <-EOF  
 				#[Member]
@@ -82,6 +88,9 @@ etcd_conf(){
 		for host in ${host_name[@]};
 		do
 			if [[ ${host} = '${etcd_ip[$j]}' ]];then
+				send_file
+				install_etcd
+				add_system
 				ssh ${host_name[$i]} -p ${ssh_port[$i]}
 				cat >>${etcd_dir}/cfg/etcd.conf <-EOF  
 				#[Member]
@@ -137,9 +146,8 @@ k8s_bin_install(){
 	install_cfssl
 	create_etcd_ca
 	down_k8s_file
-	send_file
-	install_etcd
+	
 	etcd_conf
-	add_system
+	
 
 }
