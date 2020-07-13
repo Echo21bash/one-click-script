403f6ebd21d6f2b010b451673210489904d2a140
hebaodan <hebaodan21@qq.com>
2020-07-13 19:15:13 +0800

    updata

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 down_file(){
-	github_mirror=(https://download.fastgit.org https://hub.fastgit.org https://github.wuyanzheshui.workers.dev https://github.com.cnpmjs.org)
+	github_mirror=(https://download.fastgit.org https://github.wuyanzheshui.workers.dev https://hub.fastgit.org https://github.com.cnpmjs.org)
 	#$1下载链接、$2保存路径及名称
 	if [[ -n $1 && -n $2 ]];then
 		down_url=$1
