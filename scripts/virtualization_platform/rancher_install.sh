8376dd454560f73485875a52d013330039e3982a
hebaodan <hebaodan21@qq.com>
2020-09-23 13:37:26 +0800

    updata

--- scripts/virtualization_platform/rancher_install.sh
+++ scripts/virtualization_platform/rancher_install.sh
@@ -4,8 +4,13 @@ rancher_install_ctl(){
 	if [[ -z `which dockerd 2>/dev/null` ]];then
 		docker_install
 	fi
+	get_ip
 	docker run -itd --name rancher --restart=unless-stopped \
-	-p 8888:80 \
+	-p 8888:443 \
 	-v /opt/rancher:/var/lib/rancher \
 	rancher/rancher:latest
+	if [[ $? = '0' ]];then
+		sleep 10
+		diy_echo "通过Docker部署完成，可通过https:${local_ip}:8888访问" "${info}"
+	fi
 }
