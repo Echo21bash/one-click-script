4d6d92999c7f0cfd6901e1ab0fe4a661f5c54d96
hebaodan <hebaodan21@qq.com>
2020-10-15 14:08:27 +0800

    updata

--- scripts/db_server/redis_install.sh
+++ scripts/db_server/redis_install.sh
@@ -32,15 +32,9 @@ redis_install_set(){
 	fi
 		
 	if [[ ${cluster_mode} = '1' ]];then
-		input_option '输入本机部署个数' '2' 'deploy_num'
-		only_allow_numbers ${deploy_num}
-		if [[ $? = 1 ]];then
-			echo -e "${error} 输入错误请重新设置"
-			redis_install_set
-		fi
-		input_option '输入起始端口号' '7001' 'redis_port'
-		input_option '请设置redis密码' 'passw0ord' 'redis_password'
-		redis_password=${input_value}
+		vi ${work_dir}/config/redis/redis_cluster.conf
+		. ${work_dir}/config/redis/redis_cluster.conf
+
 	fi
 
 	if [[ ${cluster_mode} = '2' ]];then
@@ -70,56 +64,73 @@ redis_install_set(){
 			input_option '输入起始端口号' '7001' 'redis_port'
 		fi
 	fi
-	diy_echo '按任意键继续' '' "$info"
-	read
 
 }
 
 redis_install(){
 
-	echo -e "${info} 正在安装编译工具及库文件..."
+	info_log "正在安装编译工具及库文件..."
 	yum -y install make  gcc-c++
 	if [ $? = '0' ];then
-		echo -e "${info} 编译工具及库文件安装成功."
+		info_log "编译工具及库文件安装成功."
 	else
-		echo -e "${error} 编译工具及库文件安装失败请检查!!!" && exit 1
+		error_log "编译工具及库文件安装失败请检查!!!" && exit 1
 	fi
 	if [[ ${deploy_mode} = '2' && ${cluster_mode} = '1' && ${version_number} < '5.0' ]];then
 		if [[ $(which ruby 2>/dev/null)  &&  $(ruby -v | grep -oE "[0-9]{1}\.[0-9]{1}\.[0-9]{1}") > '2.2.2' ]];then
 			gem install redis
 		else
-			diy_echo "ruby未安装或者版本低于2.2.2" "" "${error}"
+			error_log "ruby未安装或者版本低于2.2.2"
 			exit 1
 		fi
 	fi
-	home_dir=${install_dir}/redis
-	if [[ ! -d ${home_dir} ]];then
-		mkdir -p ${home_dir}
-	fi
+	###编译redis
+	make_home_dir=${tmp_dir}/redis
 	cd ${tar_dir}
-	make && cd ${tar_dir}/src && make PREFIX=${home_dir} install
+	make && cd ${tar_dir}/src && make PREFIX=${make_home_dir} install
 	if [ $? = '0' ];then
-		echo -e "${info} Redis安装完成"
+		info_log "Redis编译完成"
 	else
-		echo -e "${error} Redis编译失败!" && exit
+		error_log "Redis编译失败!" && exit
 	fi
 
 	if [[ ${deploy_mode} = '1' ]];then
+		home_dir=${install_dir}/redis
+		mkdir -p ${home_dir}
+		cp -rp ${make_home_dir}/* ${home_dir}
 		redis_config
 		add_redis_service
 		add_sys_env "PATH=${home_dir}/bin:\$PATH"
 	elif [ ${deploy_mode} = '2' ];then
-		if [[ ${cluster_mode} = '2' && ${node_type} = 'M' ]];then
-			mast_redis_port=${redis_port}
-		fi
-		mv ${home_dir} ${install_dir}/tmp
-		for ((i=1;i<=${deploy_num};i++))
+		auto_ssh_keygen
+		local i=1
+		local k=0
+		for now_host in ${host_ip[@]}
 		do
-			\cp -rp ${install_dir}/tmp ${install_dir}/redis-${redis_port}
-			home_dir=${install_dir}/redis-${redis_port}
-			redis_config
-			add_redis_service
-			redis_port=$((${redis_port}+1))
+			redis_port=6379
+			for ((j=0;j<${node_num[$k]};j++))
+			do
+				service_id=$i
+				let redis_port=${redis_port}+$j
+				redis_config
+				home_dir=${install_dir}/redis-node${service_id}				
+				add_redis_service
+				ssh ${host_ip[$k]} -p ${ssh_port[$k]} "
+				mkdir -p ${install_dir}/redis-node${service_id}
+				mkdir -p ${redis_data_dir}/node${service_id}
+				"
+				info_log "正在向节点${now_host}分发redis-node${service_id}安装程序和配置文件..."
+				scp -q -r -P ${ssh_port[$k]} ${make_home_dir}/* ${host_ip[$k]}:${install_dir}/redis-node${service_id}
+				scp -q -r -P ${ssh_port[$k]} ${tmp_dir}/{redis-node${i}.service,log_cut_redis-node${i}} ${host_ip[$k]}:${install_dir}/redis-node${service_id}
+				
+				ssh ${host_ip[$k]} -p ${ssh_port[$k]} "
+				\cp ${install_dir}/redis-node${service_id}/redis-node${i}.service /etc/systemd/system/redis-node${i}.service
+				\cp ${install_dir}/redis-node${service_id}/log_cut_redis-node${i} /etc/rsyslog.d/redis-node${i}
+				systemctl daemon-reload
+				"
+				((i++))
+			done
+			((k++))
 		done
 		add_sys_env "PATH=${home_dir}/bin:\$PATH"
 		redis_cluster_description
@@ -127,47 +138,59 @@ redis_install(){
 }
 
 redis_config(){
-	get_ip
-	mkdir -p ${home_dir}/{logs,etc,data}
-	conf_dir=${home_dir}/etc
+	#public config
+	conf_dir=${make_home_dir}
 	cp ${tar_dir}/redis.conf ${conf_dir}/redis.conf
-	sed -i "s/^bind.*/bind 127.0.0.1 ${local_ip}/" ${conf_dir}/redis.conf
-	sed -i 's/^port 6379/port '${redis_port}'/' ${conf_dir}/redis.conf
+	sed -i "s/^port .*/port ${redis_port}/" ${conf_dir}/redis.conf
 	sed -i 's/^daemonize no/daemonize yes/' ${conf_dir}/redis.conf
-	sed -i "s#^pidfile .*#pidfile ${home_dir}/data/redis.pid#" ${conf_dir}/redis.conf
-	sed -i 's#^logfile ""#logfile "'${home_dir}'/logs/redis.log"#' ${conf_dir}/redis.conf
-	sed -i 's#^dir ./#dir '${home_dir}'/data#' ${conf_dir}/redis.conf
-	sed -i 's/# requirepass foobared/requirepass '${redis_password}'/' ${conf_dir}/redis.conf
+	sed -i "s#^dir .*#dir ${redis_data_dir}#" ${conf_dir}/redis.conf
+	sed -i "s/# requirepass foobared/requirepass ${redis_password}/" ${conf_dir}/redis.conf
 	sed -i 's/# maxmemory <bytes>/maxmemory 100mb/' ${conf_dir}/redis.conf
 	sed -i 's/# maxmemory-policy noeviction/maxmemory-policy volatile-lru/' ${conf_dir}/redis.conf
 	sed -i 's/appendonly no/appendonly yes/' ${conf_dir}/redis.conf
 	
-	if [ ${deploy_mode} = '1' ];then
-		add_log_cut redis ${home_dir}/logs/*.log
-	elif [ ${deploy_mode} = '2' ];then
-		if [[ ${cluster_mode} = '1' ]];then
-			mkdir -p ${install_dir}/bin
-			cp ${tar_dir}/src/redis-trib.rb ${install_dir}/bin/redis-trib.rb
-			sed -i 's/^# masterauth <master-password>/masterauth '${redis_password}'/' ${conf_dir}/redis.conf
-			sed -i 's/# cluster-enabled yes/cluster-enabled yes/' ${conf_dir}/redis.conf
-			sed -i 's/# cluster-config-file nodes-6379.conf/cluster-config-file nodes-'${redis_port}'.conf/' ${conf_dir}/redis.conf
-			sed -i 's/# cluster-node-timeout 15000/cluster-node-timeout 15000/' ${conf_dir}/redis.conf
-		elif [[ ${cluster_mode} = '2' ]];then
-			cp ${tar_dir}/sentinel.conf ${conf_dir}/sentinel.conf
-			sed -i 's/^# masterauth <master-password>/masterauth '${redis_password}'/' ${conf_dir}/redis.conf
-			if [[ ${node_type} = 'M' && ${i} != '1' ]];then
-				sed -i "s/^# slaveof <masterip> <masterport>/slaveof ${mast_redis_ip} ${mast_redis_port}/" ${conf_dir}/redis.conf
-			elif [[  ${node_type} = 'S' ]];then
-				sed -i "s/^# slaveof <masterip> <masterport>/slaveof ${mast_redis_ip} ${mast_redis_port}/" ${conf_dir}/redis.conf
-			fi
-			#哨兵配置文件
-			sed -i "s/^# bind.*/bind 127.0.0.1 ${local_ip}/" ${conf_dir}/sentinel.conf
-			sed -i "s/^port 26379/port 2${redis_port}/" ${conf_dir}/sentinel.conf
-			sed -i "s#^dir /tmp#dir ${home_dir}/data\nlogfile ${home_dir}/log/sentinel.log\npidfile ${home_dir}/data/redis_sentinel.pid\ndaemonize yes#" ${conf_dir}/sentinel.conf
-			sed -i "s#^sentinel monitor mymaster 127.0.0.1 6379 2#sentinel monitor mymaster ${local_ip} ${mast_redis_port} 2#" ${conf_dir}/sentinel.conf
-			sed -i 's!^# sentinel auth-pass mymaster.*!sentinel auth-pass mymaster '${redis_password}'!' ${conf_dir}/sentinel.conf
+	if [[ ${deploy_mode} = '1' ]];then
+		get_ip
+		mkdir -p ${home_dir}/{logs,etc,data}
+		cp -rp ${make_home_dir}/redis/* ${home_dir}
+		cp ${conf_dir}/redis.conf ${home_dir}/etc/redis.conf
+		sed -i "s/^bind.*/bind 127.0.0.1 ${local_ip}/" ${home_dir}/etc/redis.conf
+		sed -i "s#^pidfile .*#pidfile ${redis_data_dir}/redis-${redis_port}.pid#" ${home_dir}/etc/redis.conf
+		sed -i "s#^logfile .*#logfile ${home_dir}/logs/redis.log#" ${home_dir}/redis.conf
+		add_log_cut ${home_dir}/log_cut_redis ${home_dir}/logs/redis.log
+	fi
+	
+	if [[ ${deploy_mode} = '2' && ${cluster_mode} = '1' ]];then
+		cp ${tar_dir}/src/redis-trib.rb ${make_home_dir}/bin/redis-trib.rb
+		sed -i "s/^bind.*/bind 127.0.0.1 ${now_host}/" ${conf_dir}/redis.conf
+		sed -i "s#^pidfile .*#pidfile ${redis_data_dir}/node${service_id}/redis-${redis_port}.pid#" ${conf_dir}/redis.conf
+		sed -i "s#^logfile .*#logfile ${install_dir}/redis-node${service_id}/logs/redis.log#" ${conf_dir}/redis.conf
+		sed -i "s/^# masterauth <master-password>/masterauth ${redis_password}/" ${conf_dir}/redis.conf
+		sed -i "s/# cluster-enabled yes/cluster-enabled yes/" ${conf_dir}/redis.conf
+		sed -i "s/# cluster-config-file nodes-6379.conf/cluster-config-file nodes-${redis_port}.conf/" ${conf_dir}/redis.conf
+		sed -i "s/# cluster-node-timeout 15000/cluster-node-timeout 15000/" ${conf_dir}/redis.conf
+		add_log_cut ${tmp_dir}/log_cut_redis-node${service_id} ${install_dir}/redis-node${service_id}/logs/redis.log
+	fi
+	
+	if [[ ${deploy_mode} = '2' && ${cluster_mode} = '2' ]];then
+		cp ${tar_dir}/sentinel.conf ${make_home_dir}/sentinel.conf
+		
+		sed -i "s/^bind.*/bind 127.0.0.1 ${now_host}/" ${conf_dir}/redis.conf
+		sed -i "s#^pidfile .*#pidfile ${redis_data_dir}/node${service_id}/redis-${redis_port}.pid#" ${conf_dir}/redis.conf
+		sed -i "s#^logfile .*#logfile ${install_dir}/redis-node${service_id}/logs/redis.log#" ${conf_dir}/redis.conf
+		sed -i "s/^# masterauth <master-password>/masterauth ${redis_password}/" ${conf_dir}/redis.conf
+		if [[ ${node_type} = 'M' && ${i} != '1' ]];then
+			sed -i "s/^# slaveof <masterip> <masterport>/slaveof ${mast_redis_ip} ${mast_redis_port}/" ${conf_dir}/redis.conf
+		elif [[  ${node_type} = 'S' ]];then
+			sed -i "s/^# slaveof <masterip> <masterport>/slaveof ${mast_redis_ip} ${mast_redis_port}/" ${conf_dir}/redis.conf
 		fi
-		add_log_cut redis_${redis_port} ${home_dir}/logs/*.log
+		#哨兵配置文件
+		sed -i "s/^# bind.*/bind 127.0.0.1 ${now_host}/" ${conf_dir}/sentinel.conf
+		sed -i "s/^port .*/port 2${redis_port}/" ${conf_dir}/sentinel.conf
+		sed -i "s#^dir .*#dir ${redis_data_dir}\nlogfile ${install_dir}/redis-node${service_id}/logs/sentinel.log\npidfile ${install_dir}/redis-node${service_id}/redis_sentinel.pid\ndaemonize yes#" ${conf_dir}/sentinel.conf
+		sed -i "s#^sentinel monitor mymaster 127.0.0.1 6379 2#sentinel monitor mymaster ${now_host} ${mast_redis_port} 2#" ${conf_dir}/sentinel.conf
+		sed -i 's!^# sentinel auth-pass mymaster.*!sentinel auth-pass mymaster '${redis_password}'!' ${conf_dir}/sentinel.conf
+		add_log_cut ${tmp_dir}/log_cut_redis-node${service_id} ${install_dir}/redis-node${service_id}/logs/*.log
 	fi
 
 }
@@ -176,16 +199,18 @@ add_redis_service(){
 	Type="forking"
 	ExecStart="${home_dir}/bin/redis-server ${home_dir}/etc/redis.conf"
 	PIDFile="${home_dir}/data/redis.pid"
-	conf_system_service ${home_dir}/init
+
 	if [[ ${deploy_mode} = '1' ]];then
-		add_system_service redis ${home_dir}/init
+		conf_system_service ${home_dir}/redis.service
+		add_system_service redis ${home_dir}/redis.service
 	elif [[ ${deploy_mode} = '2' ]];then
-		add_system_service redis-${redis_port} ${home_dir}/init
+		conf_system_service ${tmp_dir}/redis-node${service_id}.service
+		
 		if [[ ${cluster_mode} = '2' ]];then
 			ExecStart="${home_dir}/bin/redis-sentinel ${home_dir}/etc/sentinel"
 			PIDFile="${home_dir}/data/redis_sentinel.pid"
-			conf_system_service
-			add_system_service redis-sentinel-2${redis_port} ${home_dir}/init
+			conf_system_service ${tmp_dir}/redis-sentinel-node${service_id}.service
+			add_system_service redis-sentinel-node${service_id} ${tmp_dir}/redis-sentinel-node${service_id}.service
 		fi
 	fi
 }
