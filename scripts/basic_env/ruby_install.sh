83e13f0c207cac286876de1bf8dd735c3b028cb0
hebaodan <hebaodan21@qq.com>
2020-10-16 14:48:49 +0800

    updata

--- scripts/basic_env/ruby_install.sh
+++ scripts/basic_env/ruby_install.sh
@@ -19,12 +19,10 @@ ruby_install(){
 
 	yum install -y zlib-devel openssl-devel
 	home_dir=${install_dir}/ruby
-	make_home_dir=${tmp_dir}/ruby
-	mkdir -p ${make_home_dir} ${home_dir}
+	mkdir -p ${home_dir}
 	cd ${tar_dir}
-	./configure --prefix=${make_home_dir}  --disable-install-rdoc
+	./configure --prefix=${home_dir}  --disable-install-rdoc
 	make && make install
-	cp -rp ${make_home_dir}/* ${home_dir}
 	add_sys_env "PATH=${home_dir}/bin:\$PATH"
 
 	gem sources --add http://gems.ruby-china.com/ --remove http://rubygems.org/
