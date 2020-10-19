50bacdc7cb8e7fa1839432be250105de7ba1de80
hebaodan <hebaodan21@qq.com>
2020-10-19 18:16:20 +0800

    updata

--- scripts/basic_env/java_install.sh
+++ scripts/basic_env/java_install.sh
@@ -40,7 +40,7 @@ check_java(){
 
 install_java(){
 	check_java
-	${home_dir}=${install_dir}/java
+	home_dir=${install_dir}/java
 	mkdir -p ${home_dir}
 	cp -rp ${tar_dir}/* ${home_dir}
 	add_sys_env "JAVA_HOME=${home_dir} JAVA_BIN=\$JAVA_HOME/bin JAVA_LIB=\$JAVA_HOME/lib CLASSPATH=.:\$JAVA_LIB/tools.jar:\$JAVA_LIB/dt.jar PATH=\$JAVA_HOME/bin:\$PATH"
