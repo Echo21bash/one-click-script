7890ff1afef50df6773f1304d0aea95186316723
hebaodan <hebaodan21@qq.com>
2020-10-29 13:01:28 +0800

    updata

--- scripts/operation_platform/zabbix_install.sh
+++ scripts/operation_platform/zabbix_install.sh
@@ -21,7 +21,7 @@ zabbix_set(){
 	output_option "请选择要安装的模块" "zabbix-server zabbix-agent zabbix-java zabbix-proxy" "install_module"
 	install_module_value=(${output_value[@]})
 	module_configure=$(echo ${install_module_value[@]} | sed s/zabbix/--enable/g)
-	if [[ ${install_module[@]} =~ 'zabbix-server' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-server' ]];then
 		diy_echo "现在设置zabbix-server相关配置" "${yellow}" "${info}"
 		input_option "请输入要连接的数据库地址" "127.0.0.1" "zabbix_db_host"
 		zabbix_db_host=${input_value}
@@ -33,14 +33,14 @@ zabbix_set(){
 		input_option "请输入要连接的数据库密码" "123456" "zabbix_db_passwd"
 		zabbix_db_passwd=${input_value}
 	fi
-	if [[ ${install_module[@]} =~ 'zabbix-agent' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-agent' ]];then
 		diy_echo "现在设置zabbix-agent相关配置" "${yellow}" "${info}"
 		input_option "请输入要连接的zabbix-server地址" "127.0.0.1" "zabbix_server_host"
 		zabbix_server_host=${input_value}
 		input_option "请设置zabbix-agent的主机名地址" "zabbix_server" "zabbix_agent_host_name"
 		zabbix_agent_host_name=${input_value}
 	fi
-	if [[ ${install_module[@]} =~ 'zabbix-java' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-java' ]];then
 		echo
 	fi
 }
@@ -69,7 +69,7 @@ zabbix_config(){
 	useradd zabbix -M -g zabbix -s /bin/false >/dev/null 2>&1
 	mkdir -p ${home_dir}/logs
 	chown -R zabbix.zabbix ${home_dir}/logs
-	if [[ ${install_module[@]} =~ 'zabbix-server' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-server' ]];then
 	
 		sed -i 's#^LogFile.*#LogFile='${home_dir}'/logs/zabbix_server.log#' ${home_dir}/etc/zabbix_server.conf
 		sed -i 's@^# PidFile=.*@PidFile='${home_dir}'/logs/zabbix_server.pid@' ${home_dir}/etc/zabbix_server.conf
@@ -81,7 +81,7 @@ zabbix_config(){
 		sed -i 's@^# Include=/usr/local/etc/zabbix_server.conf.d/\*\.conf@Include='${home_dir}'/etc/zabbix_server.conf.d/*.conf@' ${home_dir}/etc/zabbix_server.conf
 	fi
  
-	if [[ ${install_module[@]} =~ 'zabbix-agent' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-agent' ]];then
 
 		sed -i 's@^# PidFile=.*@PidFile='${home_dir}'/logs/zabbix_agentd.pid@' ${home_dir}/etc/zabbix_agentd.conf
 		sed -i 's#^LogFile.*#LogFile='${home_dir}'/logs/zabbix_agentd.log#' ${home_dir}/etc/zabbix_agentd.conf
@@ -89,7 +89,7 @@ zabbix_config(){
 		sed -i 's#^Hostname=.*#Hostname='${zabbix_agent_host_name}'#' ${home_dir}/etc/zabbix_agentd.conf
 		sed -i 's@^# Include=/usr/local/etc/zabbix_agentd.conf.d/\*\.conf@Include='${home_dir}'/etc/zabbix_agentd.conf.d/*.conf@' ${home_dir}/etc/zabbix_agentd.conf
 	fi
-	if [[ ${install_module[@]} =~ 'zabbix-java' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-java' ]];then
 		sed -i 's@^PID_FILE=.*@PID_FILE='${home_dir}'/logs/zabbix_java.pid@' ${home_dir}/sbin/zabbix_java/settings.sh
 		sed -i 's@/tmp/zabbix_java.log@'${home_dir}'/logs/zabbix_java.log@' ${home_dir}/sbin/zabbix_java/lib/logback.xml
 	fi
@@ -98,21 +98,21 @@ zabbix_config(){
 
 add_zabbix_service(){
 	Type="forking"
-	if [[ ${install_module[@]} =~ 'zabbix-server' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-server' ]];then
 		Environment="CONFFILE=${home_dir}/etc/zabbix_server.conf"
 		PIDFile="${home_dir}/logs/zabbix_server.pid"
 		ExecStart="${home_dir}/sbin/zabbix_server -c \$CONFFILE"
 		conf_system_service ${home_dir}/zabbix-serverd
 		add_system_service zabbix-serverd ${home_dir}/zabbix-serverd
 	fi
-	if [[ ${install_module[@]} =~ 'zabbix-agent' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-agent' ]];then
 		Environment="CONFFILE=${home_dir}/etc/zabbix_agentd.conf"
 		PIDFile="${home_dir}/logs/zabbix_agentd.pid"
 		ExecStart="${home_dir}/sbin/zabbix_agentd -c \$CONFFILE"
 		conf_system_service ${home_dir}/zabbix-agentd
 		add_system_service zabbix-agentd ${home_dir}/zabbix-agentd
 	fi
-	if [[ ${install_module[@]} =~ 'zabbix-java' ]];then
+	if [[ ${install_module_value[@]} =~ 'zabbix-java' ]];then
 		PIDFile="${home_dir}/logs/zabbix_java.pid"
 		ExecStart="${home_dir}/sbin/zabbix_java/startup.sh"
 		conf_system_service ${home_dir}/zabbix-java-gateway
