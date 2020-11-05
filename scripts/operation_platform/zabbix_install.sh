cc60bf43a9c572dc21a4c637476cf3429f1a0ffa
hebaodan <hebaodan21@qq.com>
2020-11-05 18:15:19 +0800

    updata

--- scripts/operation_platform/zabbix_install.sh
+++ scripts/operation_platform/zabbix_install.sh
@@ -48,11 +48,17 @@ zabbix_set(){
 zabbix_install(){
 
 	diy_echo "正在安装编译工具及库文件..." "" "${info}"
-	yum -y install net-snmp-devel libxml2-devel libcurl-devel mysql-devel libevent-devel
+	if [[ ${install_module_value[@]} =~ 'zabbix-server' || ${install_module_value[@]} =~ 'zabbix-proxy' ]];then
+		yum -y install gcc make net-snmp-devel libxml2-devel libcurl-devel mysql-devel libevent-devel
+		./configure --prefix=${home_dir} ${module_configure} --with-mysql --with-net-snmp --with-libcurl --with-libxml2
+	else
+		yum -y install gcc make net-snmp-devel libxml2-devel libcurl-devel libevent-devel
+		./configure --prefix=${home_dir} ${module_configure} --with-net-snmp --with-libcurl --with-libxml2
+	fi
 	cd ${tar_dir}
 	home_dir=${install_dir}/zabbix
 	mkdir -p ${home_dir}
-	./configure --prefix=${home_dir} ${module_configure} --with-mysql --with-net-snmp --with-libcurl --with-libxml2
+	
 	make && make install
 	if [ $? = '0' ];then
 		diy_echo "编译完成..." "" "${info}"
@@ -85,8 +91,10 @@ zabbix_config(){
 
 		sed -i 's@^# PidFile=.*@PidFile='${home_dir}'/logs/zabbix_agentd.pid@' ${home_dir}/etc/zabbix_agentd.conf
 		sed -i 's#^LogFile.*#LogFile='${home_dir}'/logs/zabbix_agentd.log#' ${home_dir}/etc/zabbix_agentd.conf
-		sed -i 's#^Server=.*#Server='${zabbix_server_host}'#' ${home_dir}/etc/zabbix_agentd.conf
+		sed -i 's#^ServerActive=.*#ServerActive='${zabbix_server_host}'#' ${home_dir}/etc/zabbix_agentd.conf
 		sed -i 's#^Hostname=.*#Hostname='${zabbix_agent_host_name}'#' ${home_dir}/etc/zabbix_agentd.conf
+		sed -i 's@^# Timeout=.*@Timeout=20@' ${home_dir}/etc/zabbix_agentd.conf
+
 		sed -i 's@^# Include=/usr/local/etc/zabbix_agentd.conf.d/\*\.conf@Include='${home_dir}'/etc/zabbix_agentd.conf.d/*.conf@' ${home_dir}/etc/zabbix_agentd.conf
 	fi
 	if [[ ${install_module_value[@]} =~ 'zabbix-java' ]];then
