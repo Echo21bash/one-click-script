a645d0e570c0167d221a6c315c33e81ab45fd506
hebaodan <hebaodan21@qq.com>
2020-11-10 17:38:17 +0800

    updata

--- scripts/operation_platform/elasticsearch_install.sh
+++ scripts/operation_platform/elasticsearch_install.sh
@@ -3,7 +3,7 @@
 elasticsearch_env_load(){
 	tmp_dir=/tmp/elasticsearch_tmp
 	soft_name=elasticsearch
-	program_version=('5.6' '6.1' '6.2')
+	program_version=('5' '6' '7')
 	url='https://mirrors.huaweicloud.com/elasticsearch'
 	select_version
 	install_dir_set
@@ -23,65 +23,80 @@ elasticsearch_install_set(){
 		input_option "输入http端口号" "9200" "elsearch_port"
 		input_option "输入tcp通信端口号" "9300" "elsearch_tcp_port"
 	else
-		input_option "请输入部署总个数($(diy_echo 必须是奇数 $red))" "3" "deploy_num_total"
-		input_option '请输入所有部署elsearch的机器的ip地址,第一个为本机ip(多个使用空格分隔)' '192.168.1.1 192.168.1.2' 'elsearch_ip'
-		elsearch_ip=(${input_value[@]})
-		input_option '请输入每台机器部署elsearch的个数,第一个为本机部署个数(多个使用空格分隔)' '2 1' 'deploy_num_per'
-		deploy_num_local=${deploy_num_per[0]}
-		diy_echo "如果部署在多台机器,下面的起始端口号$(diy_echo 务必一致 $red)" "$yellow" "$warning"
-		input_option "输入http端口号" "9200" "elsearch_port"
-		input_option "输入tcp通信端口号" "9300" "elsearch_tcp_port"
+		vi ${workdir}/config/elk/elastic.conf
+		. ${workdir}/config/elk/elastic.conf
 	fi
 }
 
 elasticsearch_install(){
 
-	useradd -M elsearch
 	if [[ ${deploy_mode} = '1' ]];then
+		useradd -M elsearch
+		home_dir=${install_dir}/elsearch
+		mkdir -p ${install_dir}/elsearch
 		mv ${tar_dir}/* ${home_dir}
 		chown -R elsearch.elsearch ${home_dir}
 		elasticsearch_conf
 		add_elasticsearch_service
 	fi
 	if [[ ${deploy_mode} = '2' ]];then
+		auto_ssh_keygen
 		elasticsearch_server_list
-		chown -R elsearch.elsearch ${tar_dir}
-		for ((i=1;i<=${deploy_num_local};i++))
+		
+		local i=1
+		local k=0
+		for now_host in ${host_ip[@]}
 		do
-			\cp -rp ${tar_dir} ${install_dir}/elsearch-node${i}
-			home_dir=${install_dir}/elsearch-node${i}
-			elasticsearch_conf
-			add_elasticsearch_service
-			elsearch_port=$((${elsearch_port}+1))
-			elsearch_tcp_port=$((${elsearch_tcp_port}+1))
+			elsearch_port=9200
+			elsearch_tcp_port=9300
+			for ((j=0;j<${node_num[$k]};j++))
+			do
+				service_id=$i
+				let elsearch_port=9200+$j
+				let elsearch_tcp_port=9300+$j
+				elasticsearch_conf
+				home_dir=${install_dir}/elsearch-node${service_id}				
+				add_elsearch_service
+				ssh ${host_ip[$k]} -p ${ssh_port[$k]} "
+				mkdir -p ${install_dir}/elsearch-node${service_id}
+				mkdir -p ${elsearch_data_dir}/node${service_id}
+				"
+				info_log "正在向节点${now_host}分发elsearch-node${service_id}安装程序和配置文件..."
+				scp -q -r -P ${ssh_port[$k]} ${tar_dir}/* ${host_ip[$k]}:${install_dir}/elsearch-node${service_id}
+				scp -q -r -P ${ssh_port[$k]} ${tmp_dir}/{elsearch-node${i}.service,myid_node${service_id},log_cut_elsearch-node${i}} ${host_ip[$k]}:${install_dir}/elsearch-node${service_id}
+				
+				ssh ${host_ip[$k]} -p ${ssh_port[$k]} "
+				\cp ${install_dir}/elsearch-node${service_id}/elsearch-node${i}.service /etc/systemd/system/elsearch-node${i}.service
+				\cp ${install_dir}/elsearch-node${service_id}/log_cut_elsearch-node${i} /etc/logrotate.d/elsearch-node${i}
+				systemctl daemon-reload
+				"
+				((i++))
+			done
+			((k++))
 		done
 	fi
 
 }
 
 elasticsearch_server_list(){
-
-	local i
-	local j
-	local g
-	j=0
-	g=0
-
-	for ip in ${elsearch_ip[@]}
+	local i=1
+	local k=0
+	for now_host in ${host_ip[@]}
 	do
-		for num in ${deploy_num_per[${j}]}
+		elsearch_tcp_port=9300
+		for ((j=0;j<${node_num[$k]};j++))
 		do
-			for ((i=0;i<num;i++))
-			do
-				discovery_hosts[$g]="\"${elsearch_ip[$j]}:$(((elsearch_tcp_port+$i)))\","
-				g=$(((${g}+1)))
-			done	
+			service_id=$i
+			let elsearch_tcp_port=${elsearch_tcp_port}+$j
+			if [[ ${service_id} = '1' ]];then
+				discovery_hosts=
+			fi
+			discovery_hosts="${now_host}:${elsearch_tcp_port},${discovery_hosts}"
+			((i++))
 		done
-		j=$(((${j}+1)))
+		((k++))
 	done
-	#将最后一个值得逗号去掉
-	discovery_hosts[$g-1]=$(echo ${discovery_hosts[$g-1]} | grep -Eo "[\"\.0-9:]{1,}")
-	discovery_hosts=$(echo ${discovery_hosts[@]})
+
 }
 
 elasticsearch_conf(){
@@ -92,26 +107,36 @@ elasticsearch_conf(){
 		sed -i "s/#network.host.*/network.host: ${local_ip}/" ${conf_dir}/elasticsearch.yml
 		sed -i "s/#http.port.*/http.port: ${elsearch_port}\nhttp.cors.enabled: true\nhttp.cors.allow-origin: \"*\"\ntransport.tcp.port: ${elsearch_tcp_port}/" ${conf_dir}/elasticsearch.yml
 	else
-		conf_dir=${home_dir}/config
-
-		sed -i "s/#cluster.name.*/cluster.name: my-elsearch-cluster/" ${conf_dir}/elasticsearch.yml
-		sed -i "s/#node.name.*/node.name: ${local_ip}_node${i}\nnode.max_local_storage_nodes: 3/" ${conf_dir}/elasticsearch.yml
+		conf_dir=${tar_dir}/config
+		if [[ ! -f ${conf_dir}/elasticsearch.yml ]];then
+			cp ${conf_dir}/elasticsearch.yml ${conf_dir}/elasticsearch.yml.bak
+		fi
+		\cp ${conf_dir}/elasticsearch.yml.bak ${conf_dir}/elasticsearch.yml
+		sed -i "s/#cluster.name.*/cluster.name: ${elsearch-cluster}/" ${conf_dir}/elasticsearch.yml
+		sed -i "s/#node.name.*/node.name: node${service_id}\nnode.max_local_storage_nodes: 3/" ${conf_dir}/elasticsearch.yml
 		sed -i "s/#bootstrap.memory_lock.*/#bootstrap.memory_lock: false\nbootstrap.system_call_filter: false/" ${conf_dir}/elasticsearch.yml
-		sed -i "s/#network.host.*/network.host: ${local_ip}/" ${conf_dir}/elasticsearch.yml
+		sed -i "s/#network.host.*/network.host: ${now_host}/" ${conf_dir}/elasticsearch.yml
 		sed -i "s/#http.port.*/http.port: ${elsearch_port}\nhttp.cors.enabled: true\nhttp.cors.allow-origin: \"*\"\ntransport.tcp.port: ${elsearch_tcp_port}/" ${conf_dir}/elasticsearch.yml
+		sed -i "s/#discovery.seed_hosts:.*/discovery.seed_hosts: ${ES_CLUSTER_HOSTS}\ndiscovery.zen.ping_timeout: 30s/" ${conf_dir}/elasticsearch.yml
 		sed -i "s/#discovery.zen.ping.unicast.hosts.*/discovery.zen.ping.unicast.hosts: [${discovery_hosts}]\ndiscovery.zen.ping_timeout: 30s/" ${conf_dir}/elasticsearch.yml
-		sed -i "s/-Xms.*/-Xms512m/" ${conf_dir}/jvm.options
-		sed -i "s/-Xmx.*/-Xmx512m/" ${conf_dir}/jvm.options
+		sed -i "s/-Xms.*/-Xms${jvm_heap}/" ${conf_dir}/jvm.options
+		sed -i "s/-Xmx.*/-Xmx${jvm_heap}/" ${conf_dir}/jvm.options
 	fi
 
 }
 
 add_elasticsearch_service(){
+	if [[ ${deploy_mode} = '1' ]];then
+		JAVA_HOME=${JAVA_HOME}
+	else
+		JAVA_HOME=`ssh ${host_ip[$k]} -p ${ssh_port[$k]} 'echo $JAVA_HOME'`
+	fi
+	
 	Type=forking
 	User=elsearch
 	ExecStart="${home_dir}/bin/elasticsearch"
 	ARGS="-d"
-	Environment="JAVA_HOME=$(echo $JAVA_HOME)"
+	Environment="JAVA_HOME=${JAVA_HOME}"
 	conf_system_service
 
 	if [[ ${deploy_mode} = '1' ]];then
