cf4049182fc3d3ed89edc6bd6e3ee99c784ab5e6
hebaodan <hebaodan21@qq.com>
2020-11-10 19:43:06 +0800

    update

--- scripts/public/c_get_detail_version_number.sh
+++ scripts/public/c_get_detail_version_number.sh
@@ -55,7 +55,7 @@ ver_rule_general(){
 			cat ${tmp_dir}/tmp_version | grep -Eio "${version_number}u[0-9]{1,3}-b[0-9]{2}" | sort -u >${tmp_dir}/all_version
 		;;
 		
-		php|ruby|nginx|mongodb|redis|zookeeper|kafka|zabbix|elasticsearch|logstash|kibana|filebeat)
+		php|ruby|nginx|mongodb|redis|zookeeper|kafka|zabbix|logstash|kibana|filebeat)
 			cat ${tmp_dir}/tmp_version | grep -Eio "${version_number}\.[0-9]{1,2}" | sort -u >${tmp_dir}/all_version
 		;;
 		mysql)
@@ -68,7 +68,7 @@ ver_rule_general(){
 		greenplum)
 			cat ${tmp_dir}/tmp_version | grep -Eio "/${version_number}\.[0-9]{1,2}\.[0-9]{1,2}" | grep -Eio "${version_number}\.[0-9]{1,2}\.[0-9]{1,2}" | sort -u >${tmp_dir}/all_version
 		;;
-		*|node|openresty)
+		*|node|openresty|elasticsearch)
 			cat ${tmp_dir}/tmp_version | grep -Eio "${version_number}\.[0-9]{1,2}\.[0-9]{1,2}" | sort -u >${tmp_dir}/all_version
 		;;
 	esac
