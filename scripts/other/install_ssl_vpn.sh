6f18efa4243bc51b12d0d114c9dbed3d36a0b13d
hebaodan <hebaodan21@qq.com>
2021-01-11 18:31:47 +0800

    updata

--- scripts/other/install_ssl_vpn.sh
+++ scripts/other/install_ssl_vpn.sh
@@ -12,6 +12,11 @@ anylink_env_load(){
 	install_dir_set
 }
 
+anylink_set(){
+	vi ${workdir}/config/anylink/anylink.conf
+	. ${workdir}/config/anylink/anylink.conf
+}
+
 anylink_down(){
 	
 	down_url="${url}/releases/download/v${detail_version_number}/anylink-deploy-v${detail_version_number}.tar.gz"
@@ -23,10 +28,24 @@ anylink_install(){
 	mkdir -p ${home_dir}
 	unpacking_file ${tmp_dir}/anylink-deploy-v${detail_version_number}.tar.gz ${tmp_dir}
 	cp -rp ${tar_dir}/* ${home_dir}
+	chmod -R +x ${home_dir}/bin
 }
 
 anylink_config(){
-
+	cd ${home_dir}/conf
+	#自签证书
+	#openssl genrsa -des3 -out vpn.key 2048
+	#openssl rsa -in vpn.key -out vpn.key
+	#openssl req -utf8 -x509 -new -nodes -key vpn.key -sha256 -days 36500 -out vpn.pem
+	#openssl pkcs12 -export -in vpn.pem -inkey vpn.key -out vpn.p12
+	admin_pass=`${home_dir}/bin/anylink -passwd ${admin_passwd} | awk -F : '{print $2}'`
+	jwt_secret=`${home_dir}/bin/anylink -secret | awk -F : '{print $2}'`
+	sed -i "s/issuer =.*/issuer = ${vpn_name}/" server.toml
+	sed -i "s/link_addr =.*/link_addr = \"${link_addr}\"/" server.toml
+	sed -i "s/server_addr =.*/server_addr = \"${server_addr}\"/" server.toml
+	sed -i "s/admin_addr =.*/admin_addr = \"${admin_addr}\"/" server.toml
+	sed -i "s/admin_pass =.*/admin_pass = \"${admin_pass}\"/" server.toml
+	sed -i "s/jwt_secret =.*/jwt_secret = \"${jwt_secret}\"/" server.toml
 	ip_forward=$(cat /etc/sysctl.conf | grep 'net.ipv4.ip_forward = 1')
 	if [[ -z ${ip_forward} ]];then
 		echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
@@ -47,6 +66,7 @@ add_anylink_service(){
 anylink_install_ctl(){
 	anylink_env_check
 	anylink_env_load
+	anylink_set
 	anylink_down
 	anylink_install
 	anylink_config
