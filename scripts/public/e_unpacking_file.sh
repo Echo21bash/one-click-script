a80a768e899518c91d6a01be8eb3055dd3f84de6
hebaodan <hebaodan21@qq.com>
2021-03-26 17:59:47 +0800

    修复多次安装解压路径错误

--- scripts/public/e_unpacking_file.sh
+++ scripts/public/e_unpacking_file.sh
@@ -30,6 +30,9 @@ unpacking_file(){
 		package_root_dir=
 	fi
 	#解压文件
+	if [[ -d ${unpack_dir}/${package_root_dir} ]];then
+		rm -rf ${unpack_dir}/${package_root_dir}
+	fi
 	info_log "正在解压文件${unpack_file_name}到${unpack_dir}"
 	if [[ ! -d ${unpack_dir} ]];then
 		mkdir -p ${unpack_dir}
