dc0814bedf70191bed06a37c606c242e59a8929b
hebaodanroot <rootww21@gmail.com>
2021-09-16 16:16:08 +0800

    新增ES节点类型

--- scripts/operation_platform/elasticsearch_install.sh
+++ scripts/operation_platform/elasticsearch_install.sh
@@ -45,10 +45,8 @@ elasticsearch_install(){
 	fi
 	if [[ ${deploy_mode} = '2' ]];then
 		auto_ssh_keygen
-		elasticsearch_server_list
-		if [[ ${version_number} > 6 ]]; then
-			elasticsearch_master_node_list
-		fi
+		elasticsearch_master_node_list
+		elasticsearch_master_server_list
 		local i=1
 		local k=0
 		for now_host in ${host_ip[@]}
@@ -85,7 +83,7 @@ elasticsearch_install(){
 
 }
 
-elasticsearch_server_list(){
+elasticsearch_master_server_list(){
 	local i=1
 	local k=0
 	for now_host in ${host_ip[@]}
@@ -94,11 +92,12 @@ elasticsearch_server_list(){
 		for ((j=0;j<${node_num[$k]};j++))
 		do
 			service_id=$i
-			let elsearch_tcp_port=${elsearch_tcp_port}+$j
-			if [[ ${service_id} = '1' ]];then
-				discovery_hosts=
+			if [[ ${service_id} > "${master_nodes_num}" ]];then
+				break
 			fi
+			let elsearch_tcp_port=${elsearch_tcp_port}+$j
 			discovery_hosts="${now_host}:${elsearch_tcp_port},${discovery_hosts}"
+
 			((i++))
 		done
 		((k++))
@@ -107,18 +106,47 @@ elasticsearch_server_list(){
 }
 
 elasticsearch_master_node_list(){
-
-	node_total_num=${#host_ip[*]}
+	###节点总数
+	node_total_num=0
+	for num in ${node_num[@]}
+	do
+		let node_total_num=${node_total_num}+${num}
+	done
+	###未指定主节点数量时将所有节点同时配置为数据节点和主节点
 	if [[ -z ${master_nodes_num} ]];then
 		master_nodes_num=${node_total_num}
+		data_nodes_num=${node_total_num}
+		local j=0
+		for ((i=1;i<=${node_total_num};i++))
+		do
+			service_id=$i
+			master_nodes="node${service_id},${master_nodes}"
+			master_nodes_list[$j]="node${service_id}"
+			data_nodes_list[$j]="node${service_id}"
+			((j++))
+		done
+	fi
+	###指定主节点数量后主节点不存储数据，其余节点配置为数据节点
+	if [[ -n ${master_nodes_num} ]];then
+		local j=0
+		local k=0
+		for ((i=1;i<=${node_total_num};i++))
+		do
+			if [[ $i -le ${master_nodes_num} ]];then
+				service_id=$i
+				master_nodes="node${service_id},${master_nodes}"
+				master_nodes_list[$j]="node${service_id}"
+				((j++))
+			else
+				service_id=$i
+				data_nodes_list[$k]="node${service_id}"
+				((k++))
+			fi
+		done
 	fi
 
-	master_nodes=
-	for ((i=1;i<=${master_nodes_num};i++))
-	do
-		service_id=$i
-		master_nodes="node${service_id},${master_nodes}"
-	done
+
+
 
 }
 
@@ -134,14 +162,26 @@ elasticsearch_conf(){
 		if [[ ! -f ${conf_dir}/elasticsearch.yml.bak ]];then
 			cp ${conf_dir}/elasticsearch.yml ${conf_dir}/elasticsearch.yml.bak
 		fi
-		\cp ${conf_dir}/elasticsearch.yml.bak ${conf_dir}/elasticsearch.yml   
+		\cp ${conf_dir}/elasticsearch.yml.bak ${conf_dir}/elasticsearch.yml
+		###集群相关参数配置
 		sed -i "s/#cluster.name.*/cluster.name: ${cluster_name}/" ${conf_dir}/elasticsearch.yml
+		sed -i "/cluster.name.*/acluster.max_shards_per_node: 100000" ${conf_dir}/elasticsearch.yml
+		###节点参数配置
 		sed -i "s/#node.name.*/node.name: node${service_id}\nnode.max_local_storage_nodes: 3/" ${conf_dir}/elasticsearch.yml
+		if [[ ${master_nodes_list[@]} =~ "node${service_id}" ]];then
+			sed -i "/node.name.*/anode.master: true\nnode.data: false" ${conf_dir}/elasticsearch.yml
+		fi
+		if [[ ${data_nodes_list[@]} =~ "node${service_id}" ]];then
+			sed -i "/node.name.*/anode.master: false\nnode.data: true" ${conf_dir}/elasticsearch.yml
+		fi		
 		sed -i "s/#bootstrap.memory_lock.*/#bootstrap.memory_lock: false\nbootstrap.system_call_filter: false/" ${conf_dir}/elasticsearch.yml
 		sed -i "s/#network.host.*/network.host: ${now_host}/" ${conf_dir}/elasticsearch.yml
 		sed -i "s/#http.port.*/http.port: ${elsearch_port}\nhttp.cors.enabled: true\nhttp.cors.allow-origin: \"*\"\ntransport.tcp.port: ${elsearch_tcp_port}/" ${conf_dir}/elasticsearch.yml
+		###JVM内存配置
 		sed -i "s/## -Xms.*/-Xms${jvm_heap}/" ${conf_dir}/jvm.options
 		sed -i "s/## -Xmx.*/-Xmx${jvm_heap}/" ${conf_dir}/jvm.options
+		sed -i "s/-Xms.*/-Xms${jvm_heap}/" ${conf_dir}/jvm.options
+		sed -i "s/-Xmx.*/-Xmx${jvm_heap}/" ${conf_dir}/jvm.options
 		if [[ ${version_number} < 6 ]]; then
 			sed -i "s/#discovery.zen.ping.unicast.hosts:.*/discovery.zen.ping.unicast.hosts: [${discovery_hosts}]/" ${conf_dir}/elasticsearch.yml
         else
