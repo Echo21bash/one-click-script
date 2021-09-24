902280561582f2b0e19b40d829c27925a7721566
hebaodanroot <rootww21@gmail.com>
2021-09-24 12:39:38 +0800

    修复sftp

--- scripts/storage_service/sftp_install.sh
+++ scripts/storage_service/sftp_install.sh
@@ -5,12 +5,12 @@ sftp_install_ctl(){
 	input_option "请输入要SFTP家目录:" "/data/sftp" "sftp_dir"
 	sftp_dir=${input_value}
 	#set sftp homedir
-	mkdir -p ${sftp_dir}
+	mkdir -p ${sftp_dir}/${name}
 	#父目录
 	dname=$(dirname ${sftp_dir})
 	groupadd sftp_group>/dev/null 2>&1
 	usermod -G sftp_group -d ${dname} -s /sbin/nologin ${name}>/dev/null 2>&1
-	chown -R ${name}.sftp_group ${sftp_dir}
+	chown -R ${name}.sftp_group ${sftp_dir}/${name}
 	sed -i 's[^Subsystem.*sftp.*/usr/libexec/openssh/sftp-server[#Subsystem	sftp	/usr/libexec/openssh/sftp-server[' /etc/ssh/sshd_config
 	if [[ -z `grep -E '^ForceCommand    internal-sftp' /etc/ssh/sshd_config` ]];then
 		cat >>/etc/ssh/sshd_config<<-EOF
