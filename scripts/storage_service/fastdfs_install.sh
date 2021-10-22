7f2c2a6216ad7cf33ad40dfada291586cf40eb86
hebaodanroot <rootww21@gmail.com>
2021-10-22 20:36:50 +0800

    修复

--- scripts/storage_service/fastdfs_install.sh
+++ scripts/storage_service/fastdfs_install.sh
@@ -42,9 +42,9 @@ fastdfs_install(){
 	sed -i 's#TARGET_PREFIX=.*#TARGET_PREFIX=$DESTDIR#' ./make.sh
 	./make.sh  && ./make.sh install
 	if [[ $? = '0' ]];then
-		success_log "libfastcommon安装完成"
+		success_log "libfastcommon编译完成"
 	else
-		error_log "libfastcommon安装失败"
+		error_log "libfastcommon编译失败"
 		exit 1
 	fi
 	ln -sfn ${home_dir}/include/fastcommon /usr/include
@@ -57,12 +57,12 @@ fastdfs_install(){
 	sed -i 's#TARGET_CONF_PATH=.*#TARGET_CONF_PATH=$DESTDIR/etc#' ./make.sh
 	sed -i 's#TARGET_INIT_PATH=.*#TARGET_INIT_PATH=$DESTDIR/etc/init.d#' ./make.sh
 
-	info_log "正在安装fastdfs服务..."
+	info_log "正在编译fastdfs服务..."
 	./make.sh && ./make.sh install
 	if [[ $? = '0' ]];then
-		success_log "fastdfs安装完成"
+		success_log "fastdfs编译完成"
 	else
-		error_log "fastdfs安装失败"
+		error_log "fastdfs编译失败"
 		exit 1
 	fi
 	ln -sfn ${home_dir}/include/fastdfs /usr/include
@@ -82,62 +82,71 @@ fastdfs_install(){
 		sed -i 's#TARGET_INIT_PATH=.*#TARGET_INIT_PATH=$DESTDIR/etc/init.d#' ./make.sh
 		./make.sh && ./make.sh install
 		if [[ $? = '0' ]];then
-			success_log "fastdht安装完成."
+			success_log "fastdht编译完成."
 		else
-			error_log "fastdht安装失败."
+			error_log "fastdht编译失败."
 			exit 1
 		fi
+	fi
+	
+	if [[ ${deploy_mode} = '1' ]];then
+		fastdfs_config
+		add_fastdfs_service
 	fi
 }
 
 fastdfs_config(){
-	mkdir -p ${data_dir}
-	cp ${home_dir}/etc/tracker.conf.sample ${home_dir}/etc/tracker.conf
-	cp ${home_dir}/etc/storage.conf.sample ${home_dir}/etc/storage.conf
-	cp ${home_dir}/etc/client.conf.sample ${home_dir}/etc/client.conf
-	cp ${tmp_dir}/fastdfs-${detail_version_number}/conf/http.conf ${home_dir}/etc
-	cp ${tmp_dir}/fastdfs-${detail_version_number}/conf/mime.types ${home_dir}/etc
-	cp ${workdir}/config/fastdfs/fastdfs_start.sh ${home_dir}/bin/start.sh && chmod +x ${home_dir}/bin/start.sh
-	get_ip
 
-	sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/client.conf
-	sed -i "s#^tracker_server.*#tracker_server=${tracker_ip}#" ${home_dir}/etc/client.conf
+	if [[ ${deploy_mode} = '1' ]];then
+		mkdir -p ${data_dir}
+		cp ${home_dir}/etc/tracker.conf.sample ${home_dir}/etc/tracker.conf
+		cp ${home_dir}/etc/storage.conf.sample ${home_dir}/etc/storage.conf
+		cp ${home_dir}/etc/client.conf.sample ${home_dir}/etc/client.conf
+		cp ${tmp_dir}/fastdfs-${detail_version_number}/conf/http.conf ${home_dir}/etc
+		cp ${tmp_dir}/fastdfs-${detail_version_number}/conf/mime.types ${home_dir}/etc
+		get_ip
+		sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/client.conf
+		sed -i "s#^tracker_server.*#tracker_server=${local_ip}:22122#" ${home_dir}/etc/client.conf
 
 
-	sed -i "s#^port.*#port=${tracker_port}#" ${home_dir}/etc/tracker.conf
-	sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/tracker.conf
-	
-	sed -i "s#^port.*#port=${storage_port}#" ${home_dir}/etc/storage.conf
-	sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/storage.conf
-	sed -i "s#^store_path0.*#store_path0=${data_dir}#" ${home_dir}/etc/storage.conf
-	sed -i "s#^tracker_server.*#\#tracker_server=${tracker_ip}#" ${home_dir}/etc/storage.conf
-	#配置多个tracker_ip
-	len=${#fastdht_ip[@]}
-	for ((i=0;i<$len;i++))
-	do
-		sed -i "/#standard log/itracker_server=${tracker_ip[$i]}" ${home_dir}/etc/storage.conf
-	done
-	
+		sed -i "s#^port.*#port=${tracker_port}#" ${home_dir}/etc/tracker.conf
+		sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/tracker.conf
+		
+		sed -i "s#^port.*#port=${storage_port}#" ${home_dir}/etc/storage.conf
+		sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/storage.conf
+		sed -i "s#^store_path0.*#store_path0=${data_dir}#" ${home_dir}/etc/storage.conf
+		sed -i "s#^tracker_server.*#\#tracker_server=${local_ip}:22122#" ${home_dir}/etc/storage.conf
+		if [[ ${fastdht_enable} = 'yes' ]];then
+			sed -i "s#^check_file_duplicate.*#check_file_duplicate=1#" ${home_dir}/etc/storage.conf
+			sed -i "/##include /home/yuqing/fastdht/a#include ${home_dir}/etc/fdht_servers.conf" ${home_dir}/etc/storage.conf
+			#配置多个fdht_servers
+			len=${#fastdht_ip[@]}
+			echo "group_count = ${len}">${home_dir}/etc/fdht_servers.conf
+			for ((i=0;i<$len;i++))
+			do
+				echo "group0 = ${fastdht_ip[$i]}">>${home_dir}/etc/fdht_servers.conf
+			done
+		fi
 
-	if [[ ${fastdht_enable} = 'yes' ]];then
-		sed -i "s#^check_file_duplicate.*#check_file_duplicate=1#" ${home_dir}/etc/storage.conf
-		sed -i "/##include /home/yuqing/fastdht/a#include ${home_dir}/etc/fdht_servers.conf" ${home_dir}/etc/storage.conf
-		#配置多个fdht_servers
+		if [[ ${fastdht_enable} = 'yes' ]];then
+			cp ${tmp_dir}/fastdht-patch-1/conf/fdhtd.conf ${home_dir}/etc/fdhtd.conf
+			sed -i "s#^port.*#port=${fastdht_port}#" ${home_dir}/etc/fdhtd.conf
+			sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/fdhtd.conf
+		fi
+		add_log_cut fastdfs ${data_dir}/logs/*.log
+		add_sys_env "PATH=${home_dir}/bin:\$PATH"
+	fi
+
+	if [[ ${deploy_mode} = '2' ]];then
+		#配置多个tracker_ip
 		len=${#fastdht_ip[@]}
-		echo "group_count = ${len}">${home_dir}/etc/fdht_servers.conf
 		for ((i=0;i<$len;i++))
 		do
-			echo "group0 = ${fastdht_ip[$i]}">>${home_dir}/etc/fdht_servers.conf
+			sed -i "/#standard log/itracker_server=${tracker_ip[$i]}" ${home_dir}/etc/storage.conf
 		done
 	fi
 
-	if [[ ${fastdht_enable} = 'yes' ]];then
-		cp ${tmp_dir}/fastdht-patch-1/conf/fdhtd.conf ${home_dir}/etc/fdhtd.conf
-		sed -i "s#^port.*#port=${fastdht_port}#" ${home_dir}/etc/fdhtd.conf
-		sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/fdhtd.conf
-	fi
-	add_log_cut fastdfs ${data_dir}/logs/*.log
-	add_sys_env "PATH=${home_dir}/bin:\$PATH"
+
 }
 
 add_fastdfs_service(){
@@ -166,7 +175,4 @@ fastdfs_install_ctl(){
 	fastdfs_install_set
 	fastdfs_down
 	fastdfs_install
-	fastdfs_config
-	add_fastdfs_service
-	
 }
