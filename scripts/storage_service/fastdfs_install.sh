088de2875d053e3a5ef7288e00f7c4d85e0d9440
hebaodanroot <rootww21@gmail.com>
2021-10-22 20:59:09 +0800

    修复

--- scripts/storage_service/fastdfs_install.sh
+++ scripts/storage_service/fastdfs_install.sh
@@ -106,16 +106,16 @@ fastdfs_config(){
 		cp ${tmp_dir}/fastdfs-${detail_version_number}/conf/mime.types ${home_dir}/etc
 		get_ip
 		sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/client.conf
-		sed -i "s#^tracker_server.*#tracker_server=${local_ip}:22000" ${home_dir}/etc/client.conf
+		sed -i "s#^tracker_server.*#tracker_server=${local_ip}:22000#" ${home_dir}/etc/client.conf
 
 
 		sed -i "s#^port.*#port=22000#" ${home_dir}/etc/tracker.conf
 		sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/tracker.conf
 		
-		sed -i "s#^port.*#port=${storage_port}#" ${home_dir}/etc/storage.conf
+		sed -i "s#^port.*#port=23000#" ${home_dir}/etc/storage.conf
 		sed -i "s#^base_path.*#base_path=${data_dir}#" ${home_dir}/etc/storage.conf
 		sed -i "s#^store_path0.*#store_path0=${data_dir}#" ${home_dir}/etc/storage.conf
-		sed -i "s#^tracker_server.*#\tracker_server=${local_ip}:22000#" ${home_dir}/etc/storage.conf
+		sed -i "s#^tracker_server.*#tracker_server=${local_ip}:22000#" ${home_dir}/etc/storage.conf
 		if [[ ${fastdht_enable} = 'yes' ]];then
 			sed -i "s#^check_file_duplicate.*#check_file_duplicate=1#" ${home_dir}/etc/storage.conf
 			sed -i "/##include /home/yuqing/fastdht/a#include ${home_dir}/etc/fdht_servers.conf" ${home_dir}/etc/storage.conf
