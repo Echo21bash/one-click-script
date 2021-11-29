0082805630f47a2b2651df709ed5fdd8bfab9f0c
hebaodanroot <rootww21@gmail.com>
2021-11-29 16:41:45 +0800

    修复

--- scripts/other/updata_openssh.sh
+++ scripts/other/updata_openssh.sh
@@ -101,12 +101,12 @@ openssh_upgrade_batch(){
 	mkdir -p ${tmp_dir}
 	EOF
 	scp -q -r -P ${ssh_port[$k]} ${tmp_dir}/openssh* ${workdir}/bin/telshell ${workdir}/scripts/public.sh ${host_ip[$k]}:${tmp_dir}
-	ssh ${host_ip[$k]} -p ${ssh_port[$k]} <<-EOF
+	ssh ${host_ip[$k]} -p ${ssh_port[$k]} <<-'EOF'
 	. ${tmp_dir}/public.sh
 	###获取当前ssh版本
 	info_log "获取${now_host}当前ssh版本"
 	ssh_ver=`rpm -qa openssh | grep -oE "[0-9]{1}\.[0-9]{1}"`
-	if [[ \${ssh_ver} > ${version_number} || \${ssh_ver} = ${version_number} ]];then
+	if [[ ${ssh_ver} > ${version_number} || ${ssh_ver} = ${version_number} ]];then
 		info_log "现有版本大于等于选择版本无需升级"
 		exit 0
 	fi
