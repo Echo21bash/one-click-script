37954ab1a26aa7eda3bfe56ef6a5ce91c751d8cb
hebaodanroot <rootww21@gmail.com>
2021-12-22 16:44:31 +0800

    添加CentOS6源

--- scripts/other/system_optimize.sh
+++ scripts/other/system_optimize.sh
@@ -4,15 +4,11 @@ system_optimize_set(){
 
 	###yum替换为阿里源
 	[[ ! -f /etc/yum.repos.d/CentOS-Base.repo.backup ]] && cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-
 	if [[ ${os_release} < "7" ]];then
-		if [[ ! -f /etc/yum.repos.d/epel.repo ]];then
-			curl -sL -o /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo >/dev/null 2>&1
-		fi
-		if [[ -z `grep mirrors.aliyun.com /etc/yum.repos.d/CentOS-Base.repo` ]];then
-			curl -sL -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo >/dev/null 2>&1
-		fi
-	else
+		\cp ${workdir}/config/yum/CentOS6-epel.repo /etc/yum.repos.d/epel.repo
+		\cp ${workdir}/config/yum/CentOS6-Base.repo /etc/yum.repos.d/CentOS-Base.repo
+	fi
+	if [[ ${os_release} > "6" ]];then
 		if [[ ! -f /etc/yum.repos.d/epel.repo ]];then
 			curl -sL -o /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo >/dev/null 2>&1
 		fi
