ae76b402e053d8bbada8c94a86321d70b07cc3e4
hebaodanroot <rootww21@gmail.com>
2022-02-16 10:36:59 +0800

    优化ssh免密配置

--- scripts/other/auto_ssh_keygen.sh
+++ scripts/other/auto_ssh_keygen.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 auto_ssh_keygen_tool(){
-	vi ${workdir}/config/ssh/ssh.conf
-	. ${workdir}/config/ssh/ssh.conf
+	vi ${workdir}/config/ssh/passwd.txt
+	while read str
+	do
+		a=(${str})
+		host_ip=${a[0]}
+		ssh_port=${a[1]}
+		passwd=${a[2]}
+	done < ${workdir}/config/ssh/passwd.txt
 	auto_ssh_keygen
 }
