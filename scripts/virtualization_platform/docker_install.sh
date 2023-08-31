2f003a18589e9dcf965d71c576c2a39655e26bda
echo21bash <echo21bash@gmail.com>
2023-08-31 13:52:30 +0800

    更新

--- scripts/virtualization_platform/docker_install.sh
+++ scripts/virtualization_platform/docker_install.sh
@@ -2,16 +2,22 @@
 
 docker_install_ctl(){
 
-	[[ -n `which dockerd 2>/dev/null` ]] && diy_echo "检测到可能已经安装docker请检查..." "${yellow}" "${warning}" && exit 1
-	diy_echo "正在安装docker..." "" "${info}"
-	if [[ ${os_release} < "7" ]];then
-		yum install -y docker
+	if [[ -n `which dockerd 2>/dev/null` ]];then
+		diy_echo "检测到可能已经安装docker请检查..." "${yellow}" "${warning}"
+		diy_echo "正在安装docker..." "" "${info}"
 	else
-		down_file http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo /etc/yum.repos.d/docker-ce.repo
-		yum install -y docker-ce
+		if [[ ${os_release} < "7" ]];then
+			yum install -y docker
+		else
+			down_file http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo /etc/yum.repos.d/docker-ce.repo
+			yum install -y docker-ce
+		fi
+		mkdir /etc/docker
+		\cp ${workdir}/config/docker/daemon.json /etc/docker
+	fi
+	
+	if [[ -z `which docker-compose 2>/dev/null` ]];then
 		down_file https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 		chmod +x /usr/local/bin/docker-compose
 	fi
-	mkdir /etc/docker
-	\cp ${workdir}/config/docker/daemon.json /etc/docker
 }
